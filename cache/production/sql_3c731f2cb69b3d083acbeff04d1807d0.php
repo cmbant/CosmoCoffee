aca13f43a474d846822d8d8493f6a1fe93fbda88
lutsiv <lae@tangramltd.com>
2018-05-31 15:20:37 +0300

    Merge origin/master
    Conflicts:
    	application/services/Notification/Sms/Accept.php
    	application/services/Notification/Sms/AcceptAdvisor.php

--- cache/production/sql_3c731f2cb69b3d083acbeff04d1807d0.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1527763486
-SELECT forum_name FROM phpbb_forums WHERE forum_id = 13
-56
-a:1:{i:0;a:1:{s:10:"forum_name";s:14:"Early Universe";}}
--- cache/production/sql_6abb7a79411c95b6d0fb8e46483c3917.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1527765910
-SELECT question_id FROM phpbb_captcha_questions WHERE lang_iso = 'en'
-43
-a:1:{i:0;a:1:{s:11:"question_id";s:1:"1";}}
