d2d4f06396bb7104a522ad212ba16cde46307db4
lutsiv <lae@tangramltd.com>
2018-06-08 13:11:51 +0300

    Merge origin/master
    Conflicts:
    	application/services/Notification/Sms/Accept.php
    	application/services/Notification/Sms/AcceptAdvisor.php

---
+++ arxiv_cron.php
@@ -0,0 +1,166 @@
+<?php
+
+define('IN_PHPBB', true);
+
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+anti_hack($phpEx);
+
+$date = '';
+$firstpaper = true;
+$arxiv = '';
+
+do_arxiv('astro-ph');
+do_arxiv('hep-ph');
+do_arxiv('hep-th');
+do_arxiv('gr-qc');
+
+if (!empty($date)) {
+    $sql = "update phpbb_config set config_value = '$date' where config_name = 'arxiv_new_date'";
+    if (!$db->sql_query($sql)) {
+        print( 'Error adding new date');
+    }
+}
+do_arxiv('quant-ph');
+do_arxiv('physics');
+do_arxiv('math');
+do_arxiv('math-ph');
+do_arxiv('hep-ex');
+do_arxiv('cs');
+do_arxiv('stat');
+
+
+function do_arxiv($in) {
+    global $date, $arxiv;
+
+    $arxiv = $in;
+
+    $url = "http://arxiv.org/list/$arxiv/new?skip=0&show=500";
+    $html = get_url($url);
+
+    $parts = explode('<h3>', $html, 4);
+
+
+    if (preg_match('#New submissions for.*?(\d+ \w\w\w \d*)#is', $parts[1], $date_file)) {
+
+        $adate = date("Y-m-d", strtotime($date_file[1]));
+
+        if ($date && $adate < $date) {
+            return;
+        }
+
+        print "$arxiv ";
+
+        $date = $adate;
+
+        $posts = $parts[1];
+
+        if (preg_match('#Cross\-lists for#is', $parts[2])) {
+            $rest = $parts[3];
+        } else {
+            $rest = $parts[2];
+        }
+
+        do_section($posts, false);
+
+        if (preg_match('#Replacements for.*?\<DL\>(.*?)\<\/DL#is', $rest, $parts)) {
+
+            $replace = $parts[1];
+            do_section($replace, true);
+        }
+
+        $posts = $parts[2];
+    } else {
+        print ("no match: $url\n");
+    }
+}
+
+function do_section($chunk, $isreplace) {
+    $match_count = preg_match_all("#\<DT\>(.*?)\<\/DD\>#is", $chunk, $matches);
+
+    for ($i = 0; $i < $match_count; $i++) {
+        $post = $matches[0][$i];
+        parse_post($post, $isreplace);
+    }
+}
+
+function parse_post($post, $isreplace) {
+    global $db, $date, $firstpaper, $arxiv;
+
+    if ($isreplace) {
+        $match = '#arxiv:(.*?)<\/a.*?Title\:.*?\>\s*(.*?)\<.*?Authors\:.*?\>\s*(.*?)\<\/div(.*?\<\/div)#is';
+    } else {
+        $match = '#arxiv:(.*?)<\/a.*?Title\:.*?\>\s*(.*?)\<.*?Authors\:.*?\>\s*(.*?)\<\/div(.*?\<\/div).*?\<p .*?\>(.*?)\<\/p#is';
+    }
+
+    if (preg_match($match, $post, $ref)) {
+
+        $formats = $ref[1];
+        $title = doclean($ref[2]);
+
+        $authors = $ref[3];
+        $authors = preg_replace("'\<[\/\!]*[^\<\>]*?\>'si", "", $authors);
+        $authors = doclean($authors);
+        
+        $extra = $ref[4];
+        
+        $abstract = preg_replace('#\<a href\s*=\s*\"(.*?)\".*?\>\s*this http URL\s*\<\/a\s*\>#is', '\\1', $ref[5]);
+        $abstract = preg_replace("'\<[\/\!]*?[^\<\>]*?\>'si", "", $abstract);
+        $abstract = doclean($abstract);
+
+//print "$authors\n$title\n$abstract\n$extra\n\n";
+//exit;
+
+        if (preg_match('#Comments:.*?\>\s*(.*?)\<\/div#is', $extra, $ref)) {
+            $comments = $ref[1];
+            $comments = preg_replace('#\<a href\s*=\s*\"([^\"]*?)\"\>\s*this http URL\s*\<\/a\s*\>#is', '\\1', $comments);
+            $comments = doclean(preg_replace('#\<[\/\!]*?[^\<\>]*?\>#si', '', $comments));
+        } else {
+            $comments = '';
+        }
+        
+        //if (preg_match('#\/abs\/([0-9a-zA-Z\.\-\/]*)#s',$formats,$ref)){
+        if (true) {
+            $arxiv_tag = $formats;
+            $number = $arxiv_tag;
+
+            if ($firstpaper && !$isreplace) {
+                $firstpaper = false;
+                $sql = "select arxiv_tag from ARXIV_NEW where arxiv_tag ='" . $arxiv_tag . "';";
+                if (!$db->sql_query($sql)) {
+                    print( 'Error looking old');
+                }
+                if ($db->sql_fetchrow($result)) {
+                    #exit;
+                }
+            }
+
+            if ($isreplace) {
+                $sql = "REPLACE INTO ARXIV_REPLACE (arxiv_tag, date, arxiv, number,title,authors,comments) values" .
+                    " ('$arxiv_tag','$date','$arxiv','$number','$title','$authors','$comments');";
+            } else {
+                $sql = "REPLACE INTO ARXIV_NEW (arxiv_tag, date, arxiv, number,title,authors,comments,abstract) values" .
+                    " ('$arxiv_tag','$date','$arxiv','$number','$title','$authors','$comments','$abstract');";
+            }
+            if (!$db->sql_query($sql)) {
+                print( 'Error in adding paper to db');
+            }
+//print ("$arxiv_tag\n$formats\n$title\n$authors\n$comments\n$abstract"); 
+        } else {
+            print("arxiv tag fail\n");
+        }
+    } else {
+        print("Post fail match\n");
+        exit;
+    }
+}
+
+function doclean($text) {
+    $text = preg_replace('/(\s)\s+/s', '$1', $text);
+    $text = preg_replace('/\<\/b\>/is', '', $text);
+    $text = clean_sql($text);
+    return $text;
+}
+
--- includes/functions_cosmocoffee.php
+++ includes/functions_cosmocoffee.php
@@ -3,19 +3,18 @@
 function anti_hack($phpEx) {
     global $request;
 
-    if (strpos($request->server('REQUEST_URI'), "eval(") ||      
-      strpos($request->server('REQUEST_URI'), "CONCAT") ||
-      strpos($request->server('REQUEST_URI'), "UNION+SELECT") ||
-      strpos($request->server('REQUEST_URI'), "base64"))
-    { 
+    if (strpos($request->server('REQUEST_URI'), "eval(") ||
+        strpos($request->server('REQUEST_URI'), "CONCAT") ||
+        strpos($request->server('REQUEST_URI'), "UNION+SELECT") ||
+        strpos($request->server('REQUEST_URI'), "base64")) {
         redirect(append_sid("/index.$phpEx"));
     }
 }
 
 function get_username_by_id($user_id) {
     global $db;
-    
-    $result = $db->sql_query("SELECT username FROM phpbb_users WHERE user_id = " . (int)$user_id . " LIMIT 1");
+
+    $result = $db->sql_query("SELECT username FROM phpbb_users WHERE user_id = " . (int) $user_id . " LIMIT 1");
     $row = $db->sql_fetchrow($result);
     $db->sql_freeresult($result);
 
@@ -28,7 +27,7 @@ function get_username_by_id($user_id) {
  * - Goes through the given string, and replaces www.xxxx.yyyy[zzzz] with an HTML <a> tag linking
  * 	to http://www.xxxx.yyyy[/zzzz]
  * - Goes through the given string, and replaces xxxx@yyyy with an HTML mailto: tag linking
- *		to that email address
+ * 		to that email address
  * - Only matches these 2 patterns either after a space, or at the beginning of a line
  *
  * Notes: the email one might get annoying - it's easy to make it more restrictive, though.. maybe
@@ -36,70 +35,70 @@ function get_username_by_id($user_id) {
  */
 function make_clickable_cosmocoffee($text) {
     global $user;
-    
-	$text = preg_replace('#(script|about|applet|activex|chrome):#is', "\\1&#058;", $text);
 
-	// pad it with a space so we can match things at the start of the 1st line.
-	$ret = ' ' . $text;
+    $text = preg_replace('#(script|about|applet|activex|chrome):#is', "\\1&#058;", $text);
 
-	// matches an "xxxx://yyyy" URL at the start of a line, or after a space.
-	// xxxx can only be alpha characters.
-	// yyyy is anything up to the first space, newline, comma, double quote or <
-	$ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
+    // pad it with a space so we can match things at the start of the 1st line.
+    $ret = ' ' . $text;
 
-	// matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
-	// Must contain at least 2 dots. xxxx contains either alphanum, or "-"
-	// zzzz is optional.. will contain everything up to the first space, newline, 
-	// comma, double quote or <.
-	$ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret);
+    // matches an "xxxx://yyyy" URL at the start of a line, or after a space.
+    // xxxx can only be alpha characters.
+    // yyyy is anything up to the first space, newline, comma, double quote or <
+    $ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
 
-	// matches an email@domain type address at the start of a line, or after a space.
-	// Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
+    // matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
+    // Must contain at least 2 dots. xxxx contains either alphanum, or "-"
+    // zzzz is optional.. will contain everything up to the first space, newline, 
+    // comma, double quote or <.
+    $ret = preg_replace("#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret);
+
+    // matches an email@domain type address at the start of a line, or after a space.
+    // Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
     if ($user->data['user_id'] == ANONYMOUS) {
         $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1[<FONT COLOR=\"FF0000\">Log in to view email</FONT>]", $ret);
-    } else {  
+    } else {
         $ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
-    } 
+    }
 
     //astro-ph etc
     $ret = preg_replace("#(?<![\[\/\"])\b([ampncqhg][a-zA-Z\.\-]{3,}/[0-9v]{6,9})\b#i", "<a href=\"/discuss/\\1\" target=\"_blank\">\\1</a>", $ret);
     $ret = preg_replace("#(?<![\[\/\"\.\-a-z0-9;])\b(?:arxiv\:)?([0-9]{4,4}\.[0-9]{4,5}(v.)?)\b#i", "<a href=\"/discuss/\\1\" target=\"_blank\">\\1</a>", $ret);
     $ret = preg_replace("#\[([a-z][a-zA-Z\.\-]{3,}/[0-9v]{6,9})\]#i", "[<a href=\"/arxivref.php?abs=\\1\" target=\"_blank\">\\1</a>]", $ret);
     $ret = preg_replace("#\[(?:arxiv\:)?([0-9]{4,4}\.[0-9]{4,5}(v.)?)\b]#i", "[<a href=\"/arxivref.php?abs=\\1\" target=\"_blank\">\\1</a>]", $ret);
-    $ret = preg_replace("#(http://[^\s]*)\&minus;#","\\1-",$ret);
+    $ret = preg_replace("#(http://[^\s]*)\&minus;#", "\\1-", $ret);
 
-	// Remove our padding..
-	$ret = substr($ret, 1);
+    // Remove our padding..
+    $ret = substr($ret, 1);
 
-	return($ret);
+    return($ret);
 }
 
-function microtime_float() { 
-   list($usec, $sec) = explode(" ", microtime()); 
-   return ((float)$usec + (float)$sec); 
-} 
+function microtime_float() {
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float) $usec + (float) $sec);
+}
 
 function get_match_strings(array $keywords) {
     $match_str = '';
     $neg_match = '';
-    
+
     foreach ($keywords as $keyword) {
-        if($keyword) {
-            if(substr($keyword, 0, 1) === '-') {
+        if ($keyword) {
+            if (substr($keyword, 0, 1) === '-') {
                 $neg_match .= substr($keyword, 1) . '|';
             } else {
                 $match_str .= $keyword . '|';
             }
         }
-    }    
-    
-    if(!empty($match_str)) {
+    }
+
+    if (!empty($match_str)) {
         $match_str = '#\b(' . trim($match_str, '|') . ').*?\b#is';
-    }    
-    if(!empty($neg_match)) {
+    }
+    if (!empty($neg_match)) {
         $neg_match = '#\b(' . trim($neg_match, '|') . ')#is';
     }
-    
+
     return [
         'match_str' => $match_str,
         'neg_match' => $neg_match
@@ -107,30 +106,30 @@ function get_match_strings(array $keywords) {
 }
 
 function arxiv_tex($text) {
-    $realtex = false; 
-    
-    if (!(strpos($text,'$') === false) ) {
-        $realtex = true; 
-        $text = preg_replace('/\$(.*?)\$/s',' \$(\\1)\$ ', $text);
-    } 
-    
-    $text = preg_replace('/\b(?<!\\\)Omega/','\\\Omega', $text);
-    $text = preg_replace('/\b(?<!\\\)sigma/','\\\sigma', $text);
-    $text = preg_replace('/(?<!\\\)Lambda\b/','\\\Lambda', $text);  
+    $realtex = false;
 
-    $text = str_replace('\\it','', $text);
-    $text = str_replace(array('+/-','+-'), '&plusmn;', $text);
-    $text = str_replace(array('~&lt;','&lt;~'), '\alt', $text);
-    $text = str_replace(array('~&gt;','&gt;~'), '\agt', $text);
+    if (!(strpos($text, '$') === false)) {
+        $realtex = true;
+        $text = preg_replace('/\$(.*?)\$/s', ' \$(\\1)\$ ', $text);
+    }
+
+    $text = preg_replace('/\b(?<!\\\)Omega/', '\\\Omega', $text);
+    $text = preg_replace('/\b(?<!\\\)sigma/', '\\\sigma', $text);
+    $text = preg_replace('/(?<!\\\)Lambda\b/', '\\\Lambda', $text);
+
+    $text = str_replace('\\it', '', $text);
+    $text = str_replace(array('+/-', '+-'), '&plusmn;', $text);
+    $text = str_replace(array('~&lt;', '&lt;~'), '\alt', $text);
+    $text = str_replace(array('~&gt;', '&gt;~'), '\agt', $text);
     $text = str_replace('&lt;&lt;', '\\ll ', $text);
     $text = str_replace('&gt;&gt;', '\\gg ', $text);
     $text = simpletex_noBB($text, $realtex);
-    $text = str_replace(array('\\rm','\\emph','\\em'), '', $text);
-    
-    if($realtex) {
+    $text = str_replace(array('\\rm', '\\emph', '\\em'), '', $text);
+
+    if ($realtex) {
         $text = preg_replace('/ \$\((.*?)\)\$ /s', '\\1', $text);
     }
-    
+
     return $text;
 }
 
@@ -141,29 +140,29 @@ function simpletex_noBB($text, $realtex = false) {
     // \sch\"odinger etc
 
     $text = tex_accents($text);
- 
-    $specs = array('\\\\','\\{','\\}','\\^','\\_','\\$','^\\circ','^{\\circ}','\Lambda CDM');
-    $html = array('&#92;','&#123;','&#125;','&#94','&#95;','$','&deg;','&deg;','&Lambda;CDM');
-    
+
+    $specs = array('\\\\', '\\{', '\\}', '\\^', '\\_', '\\$', '^\\circ', '^{\\circ}', '\Lambda CDM');
+    $html = array('&#92;', '&#123;', '&#125;', '&#94', '&#95;', '$', '&deg;', '&deg;', '&Lambda;CDM');
+
     $text = str_replace($specs, $html, $text);
 
     //Subscripts
     $text = preg_replace('/_\{([^\}]{1,30}?\{[^\{]{0,30}?\}[^\{]{0,30}?|[^\{]{1,30}?)\}/', '<span class="texhtml"><sub>{$1}</sub></span>', $text);
 
-    if($realtex === true) {
-        $text = preg_replace('/_(\\\[a-zA-Z]+|[^\{])/' ,'<span class="texhtml"><sub>$1</sub></span>', $text);
+    if ($realtex === true) {
+        $text = preg_replace('/_(\\\[a-zA-Z]+|[^\{])/', '<span class="texhtml"><sub>$1</sub></span>', $text);
     } else {
-        $text = preg_replace('/(\\\[a-zA-Z]+|[^a-zA-Z%\/][A-Za-z]|[ ;\)]|\{\})_([\-0-9][0-9\.]*|[^\{]{1,10}?)(?=(\W|$))/S', '$1<span class="texhtml"><sub>$2</sub></span>', $text); 
+        $text = preg_replace('/(\\\[a-zA-Z]+|[^a-zA-Z%\/][A-Za-z]|[ ;\)]|\{\})_([\-0-9][0-9\.]*|[^\{]{1,10}?)(?=(\W|$))/S', '$1<span class="texhtml"><sub>$2</sub></span>', $text);
     }
 
     //Superscripts
     $text = preg_replace('/\^\{([^\}]{1,30}?\{[^\{]{0,30}?\}[^\{]{0,30}?|[^\{]{1,30}?)\}/', '<span class="texhtml"><sup>{$1}</sup></span>', $text);
-    if ($realtex === true){
-        $text = preg_replace('/\^(\\\[a-zA-Z]+|[^\{])/' ,'<span class="texhtml"><sup>$1</sup></span>', $text);
+    if ($realtex === true) {
+        $text = preg_replace('/\^(\\\[a-zA-Z]+|[^\{])/', '<span class="texhtml"><sup>$1</sup></span>', $text);
     } else {
-        $text = preg_replace('/\^\(([^\(]{1,30}?)\)/' ,'<span class="texhtml"><sup>({$1})</sup></span>', $text);
-        $text = preg_replace('/\^([\-0-9][0-9\.]*|[^\{]{1,10}?)(?=(\W|$))/' ,'<span class="texhtml"><sup>$1</sup></span>', $text);
-    }  
+        $text = preg_replace('/\^\(([^\(]{1,30}?)\)/', '<span class="texhtml"><sup>({$1})</sup></span>', $text);
+        $text = preg_replace('/\^([\-0-9][0-9\.]*|[^\{]{1,10}?)(?=(\W|$))/', '<span class="texhtml"><sup>$1</sup></span>', $text);
+    }
 
     //texify things in {}, but not \rm{} etc,\frac{}{}
     $text = preg_replace_callback('/(?<![a-zA-Z]{2})(?<!\})\{((?!\\\(it|rm|sc|text))[^\{]{1,30}?)\}/', "mathItalic", $text);
@@ -171,16 +170,16 @@ function simpletex_noBB($text, $realtex = false) {
     //Single maths letters
     $text = preg_replace('/\b([b-zB-HJ-Z]\b|A(?<![\.\?\"\!\:] .|[\.\?\"\!]  .|\n.))(?<=[\s=>\+\-\/\)\[\(,~].|&[gl]t;.)(?<!\<\/.)(?=([ \\\\&<~,\^=\+\-\/\)\(]|\W\W))(?!(\-(ray|mail)|&.(gra|acu|circ|tild|uml)))/', '<span class="texit">$1</span>', $text);
     $text = preg_replace('/([\s=>\+\-\/\)\[\(~]|&[gl]t;)([b-zB-HJ-Z]|(?<![\.\?\"\!\:] |[\.\?\"\!]  |\n)A)(?=([ \\\\&<_~\^=\+\-\/\)\(]|\W\W))(?!(\-ray|&.(uml|gra|acu|circ|tild|uml)))/', '$1<span class="texit">$2</span>', $text);
-    $text = preg_replace('/\b([aI])(?<=[=>\+\-\/\)\(\[ \n\|].)(?<!\<.|\<\/.|(in|of) a)\b(?=! \\rm)(?=([\&<_\^=\+\-\/\)\(]|\W(?!&quot)[^\"\w\<`\' ]))/' ,'<span class="texit">$1</span>', $text);
+    $text = preg_replace('/\b([aI])(?<=[=>\+\-\/\)\(\[ \n\|].)(?<!\<.|\<\/.|(in|of) a)\b(?=! \\rm)(?=([\&<_\^=\+\-\/\)\(]|\W(?!&quot)[^\"\w\<`\' ]))/', '<span class="texit">$1</span>', $text);
     $text = preg_replace('/\\\(prime|euro|dagger|deg|eth|times|alpha|Alpha|beta|Beta|chi|Chi|delta|Delta|epsilon|Epsilon|eta|Eta|gamma|Gamma|iota|Iota|kappa|Kappa|lambda|Lambda|mu|Mu|nu|Nu|omega|Omega|omicron|Omicron|phi|Phi|pi|Pi|psi|Psi|rho|Rho|sigma|Sigma|sigmaf|tau|Tau|theta|Theta|upsilon|Upsilon|xi|Xi|zeta|Zeta|and|ang|asymp|cap|cong|cup|empty|equiv|exist|forall|ge|infin|isin|le|lowast|minus|nabla|ne|ni|notin|nsub|oplus|or|otimes|part|perp|prod|prop|radic|sub|sube|sup|supe)(?=(\W|\d|$))/', '<span class="texhtml">&$1;</span>', $text);
-  
+
     // \a\b italic latex letters
     $text = preg_replace('/\\\(\w)(?=[\W_])/', '<span class="texit">$1</span>', $text);
 
     //Roman text
     $text = preg_replace('/\\\\(rm|text)\{([^\{]{1,30}?)\}/', '<span class="postbody2">$2</span>', $text);
     $text = preg_replace('/\{\\\\(rm|text) ([^\{]{1,30}?)\}/', '<span class="postbody2">$2</span>', $text);
-    $text = preg_replace('/\{\\\\sc ([^\{]{1,30}?)\}/' ,'<span class="texsc">$1</span>', $text);
+    $text = preg_replace('/\{\\\\sc ([^\{]{1,30}?)\}/', '<span class="texsc">$1</span>', $text);
 
     //Caligraphic text
     $text = preg_replace('/\\\\mathcal\{([^\{]{1,30}?)\}/', '<span class="texcal">$1</span>', $text);
@@ -196,57 +195,58 @@ function simpletex_noBB($text, $realtex = false) {
     $text = preg_replace('/\\\\(exp|\%|s(in|ec)h?|co[st]h?|tanh?|l(og|n))(?=[^a-z])/', '<span class="postbody2">$1</span>', $text);
     $text = preg_replace('/\\\(l(l|esssim|angle)|ga|hbar|sum|a(lt|gt)|odot|g(g|t?r?sim)|ell|int|rangle)(?=([^a-zA-Z]|$))/', '<img class="tex" src="/png/$1.png">', $text);
 
-    $texstrs = array('\arcsec','\wedge','\grad','\~','\simeq','``','\dots','\quad','\qquad','\ ','\pounds','---','--','\circ','\cdot','\AA','\sim','\approx','\geq','\leq','\infty','\pm','\propto','\rightarrow','\partial');
-    $htmstrs = array("&Prime;","&and;","&nabla;","~","&asymp;","&quot;","&hellip;","&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;","&pound;","&mdash;","&ndash;","&deg;","&middot;","&Aring;","~","&asymp;","&ge;","&le;","&infin;","&plusmn;","&prop;","&rarr;",'&part;');
+    $texstrs = array('\arcsec', '\wedge', '\grad', '\~', '\simeq', '``', '\dots', '\quad', '\qquad', '\ ', '\pounds', '---', '--', '\circ', '\cdot', '\AA', '\sim', '\approx', '\geq', '\leq', '\infty', '\pm', '\propto', '\rightarrow', '\partial');
+    $htmstrs = array("&Prime;", "&and;", "&nabla;", "~", "&asymp;", "&quot;", "&hellip;", "&nbsp;&nbsp;&nbsp;", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", "&nbsp;", "&pound;", "&mdash;", "&ndash;", "&deg;", "&middot;", "&Aring;", "~", "&asymp;", "&ge;", "&le;", "&infin;", "&plusmn;", "&prop;", "&rarr;", '&part;');
 
     $text = str_replace($texstrs, $htmstrs, $text);
 
     // \frac
     $text = preg_replace('/\\\\frac\{([^\{]{1,160}?)\}{([^\{]{1,160}?)\}/', '{($1)&frasl;($2)}', $text);
- 
+
     //Use minus to prevent word-breaking
-    $text = preg_replace('/\-(?=[0-9])/', '&minus;', $text); 
+    $text = preg_replace('/\-(?=[0-9])/', '&minus;', $text);
 
     // Remaining {} contain nothing {} or stuff we can't easily process 
     $text = preg_replace('/\{([^\{]*?)\}/', '$1', $text);
-           
+
     return $text;
-} 
+}
 
 function tex_accents($text) {
-    $texstrs = array('\"' => 'uml', '`'=>'grave', '\''=> 'acute', '^' => 'circ', '~'=> 'tilde', '&quot;'=> 'uml');
+    $texstrs = array('\"' => 'uml', '`' => 'grave', '\'' => 'acute', '^' => 'circ', '~' => 'tilde', '&quot;' => 'uml');
     $text = preg_replace('/\\\([\'\"\`\^\~]|&quot;)[\{]?([oeaUOAINEuni])[\}]?/e', "'&\\2'.\$texstrs['\\1'].';'", $text);
     return $text;
 }
 
-function mathItalic($matches){
+function mathItalic($matches) {
     // two or more condescutive letters in {} that are not specials or parts of HTML tags 
-    return  preg_replace('/(?!\\\[a-zA-Z]*)(?![\/&\<\"][a-zA-Z]*[\"=;\>])(^|[^a-zA-Z])([A-Za-z]{2,})/', '$1<I>$2</I>', $matches[1]);
+    return preg_replace('/(?!\\\[a-zA-Z]*)(?![\/&\<\"][a-zA-Z]*[\"=;\>])(^|[^a-zA-Z])([A-Za-z]{2,})/', '$1<I>$2</I>', $matches[1]);
 }
 
-function to_array($string, $delimiter = ',') { 
+function to_array($string, $delimiter = ',') {
     $array = explode($delimiter, $string);
     return array_map(function($item) {
         return trim($item);
     }, $array);
 }
 
-function get_url( $url ) {
-	$user_agent = 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/125.5 (KHTML, like Gecko) Safari/125.9';
+function get_url($url) {
+    $user_agent = 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/125.5 (KHTML, like Gecko) Safari/125.9';
 
-	$ch = curl_init(); 
-	//curl_setopt( $ch, CURLOPT_PROXY, $proxy );
-	curl_setopt( $ch, CURLOPT_URL, $url );
-	curl_setopt( $ch, CURLOPT_USERAGENT, $user_agent );
-	//curl_setopt( $ch, CURLOPT_COOKIEJAR, "c:\cookie.txt" );
-	curl_setopt( $ch, CURLOPT_HEADER, 1 ); 
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 ); 
-	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
-	curl_setopt( $ch, CURLOPT_TIMEOUT, 120 );
-	$result = curl_exec( $ch );
-	curl_close( $ch );
-	return $result;
+    $ch = curl_init();
+    //curl_setopt( $ch, CURLOPT_PROXY, $proxy );
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+    //curl_setopt( $ch, CURLOPT_COOKIEJAR, "c:\cookie.txt" );
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
 }
 
-
-
+function clean_sql($str) {
+    return addslashes(trim($str));
+}
