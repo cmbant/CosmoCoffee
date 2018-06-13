4816698d7f0624fee479140f8164122746cfd2eb
lutsiv <lae@tangramltd.com>
2018-06-13 14:39:38 +0300

    Merge origin/master
    Conflicts:
    	application/services/Notification/Sms/Accept.php
    	application/services/Notification/Sms/AcceptAdvisor.php

--- arxivref.php
+++ arxivref.php
@@ -1,61 +1,102 @@
 <?php
 
-// standard hack prevent
 define('IN_PHPBB', true);
-$phpbb_root_path = './';
-include($phpbb_root_path . 'extension.inc');
-include($phpbb_root_path . 'common.'.$phpEx);
 
-if ( isset($HTTP_GET_VARS['a']) ) {$data = $HTTP_GET_VARS['a']; }
- else if (isset($HTTP_POST_VARS['a']) ) {$data = $HTTP_POST_VARS['a'];}
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
 
-if ( isset($HTTP_GET_VARS['abs']) ) {$abs = $HTTP_GET_VARS['abs']; }
- else if (isset($HTTP_POST_VARS['abs']) ) {$abs = $HTTP_POST_VARS['abs'];}
+anti_hack($phpEx);
 
-if ( isset($HTTP_GET_VARS['file']) ) {$file = $HTTP_GET_VARS['file']; }
- else if (isset($HTTP_POST_VARS['file']) ) {$file = $HTTP_POST_VARS['file'];}
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+$user->get_profile_fields($user->data['user_id']);
 
 
-if (!empty($abs) || !empty($file)){
-  $userdata = session_pagestart($user_ip, PAGE_TEMPLATE);
-  init_userprefs($userdata);
-  $mirror = $userdata['user_mirror'];
-  if (empty($mirror)) {$mirror = 'arxiv.org';}
-  if (!empty($file)){
-  header('Location: ' . "http://$mirror/$file");
-  } else {
-  header('Location: ' . "http://$mirror/abs/$abs");
-  }
- exit;
+$data = $request->is_set('a') ? $request->variable('a', '') : null;
+$abs = $request->is_set('abs') ? $request->variable('abs', '') : null;
+$file = $request->is_set('file') ? $request->variable('file', '') : null;
+
+$mirror = ($user->profile_fields['pf_user_mirror']) ? $user->profile_fields['pf_user_mirror'] : $config['default_mirror'];
+
+if (!empty($file)) {
+    header('Location: ' . "http://$mirror/$file");
+    exit;
+} elseif (!empty($abs)) {
+    header('Location: ' . "http://$mirror/abs/$abs");
+    exit;
 }
 
+$sql = "select
+            phpbb_topics.topic_id 
+        from 
+            phpbb_topics, phpbb_papers 
+        where 
+            phpbb_papers.paper_id = phpbb_topics.paper_id 
+        and 
+            phpbb_papers.arxiv_tag = '$data'";
 
-$sql = "select phpbb_topics.topic_id from phpbb_topics,phpbb_papers where phpbb_papers.paper_id=phpbb_topics.paper_id and phpbb_papers.arxiv_tag='$data'";
-
-if( !($result = $db->sql_query($sql)) )
-{
-        message_die(CRITICAL_ERROR, "Could not query new paper information", "", __LINE__, __FILE__, $sql);
+if (!($result = $db->sql_query($sql))) {
+    trigger_error('Could not query new paper information');
 }
 
-if ( $row = $db->sql_fetchrow($result) )
-{
- redirect("/viewtopic.php?t=" . $row['topic_id']);
+if ($row = $db->sql_fetchrow($result)) {    
+    redirect("/viewtopic.php?t=" . $row['topic_id']);
 }
 
-
-
-// standard session management
-$userdata = session_pagestart($user_ip, PAGE_TEMPLATE);
-init_userprefs($userdata);
-
-$mirror = $userdata['user_mirror'];
-if (empty($mirror)) {$mirror = 'arxiv.org';}
- 
-if (strpos($data, 'new')){
-$arxiv = "http://$mirror/list/$data";
-} else{
-$arxiv = "http://$mirror/abs/$data";
+if (strpos($data, 'new')) {
+    $arxiv = "http://$mirror/list/$data";
+} else {
+    $arxiv = "http://$mirror/abs/$data";
 }
+
+$alternative_text = "Your browser doesn't support frames: Click on the link below to proceed to the<br><a href='$arxiv'>Arxiv</a>";
+$text = "<iframe src='$arxiv' style='border:0; height: 63vh; width:100%'>$alternative_text</iframe>";
+
+page_header("CosmoCoffee :: $data");
+$template->set_filenames(array(
+    'body' => 'message_body.html',
+));
+
+$template->assign_vars(array(
+    'ARXIV_LINKS' => get_arxiv_view_links($data),
+    'MESSAGE_TEXT' => $text
+));
+
+make_jumpbox(append_sid("{$phpbb_root_path}viewforum.$phpEx"));
+page_footer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var_dump($arxiv);
+
+// /arxiv_view.php?r=1403.398500new
+// /var/www/phpBB/cosmocoffee.loc/arxiv_view.php
+ob_start();
+?>
+
+<frameset rows="155,*" frameborder="1">
+   <frame src="/arxiv_view.php" name="fr_top" scrolling="no" border="0" noresize>
+   <frame src="https://arxiv.org/list/astro-ph/new" name="fr_bottom">
+ </frameset>
+
+<?php
+echo ob_get_clean();
+
+die('--80--');
 
 $text = '<HEAD><TITLE>CosmoCoffee :: ' . $data . '</TITLE></HEAD>
 <FRAMESET ROWS="155,*" FRAMEBORDER=1>
@@ -72,7 +113,7 @@ topmargin=5 bottomMargin=0 leftMargin=0 BORDER=0>
 <td width="500" Height=80><CENTER></CENTER></TD>
 </TR>
 <TR>
-<TD> <P>Your browser doesn\'t support frames: Click on the link below to proceed to the<BR> <A HREF="' . $arxiv. ' 
+<TD> <P>Your browser doesn\'t support frames: Click on the link below to proceed to the<BR> <A HREF="' . $arxiv . ' 
 " TARGET="_blank">Arxiv</A>.
 </TD>
 </TR>
@@ -88,6 +129,5 @@ topmargin=5 bottomMargin=0 leftMargin=0 BORDER=0>
 </HTML>';
 
 echo $text;
-
 ?>
 
